c97e8f870104308812a3fba997491c27add1f358
massbank <massbank>
2009-12-17 05:17:42 +0000

    update

--- Apache/applet/DisplayAll/compile_example.bat
+++ Apache/applet/DisplayAll/compile_example.bat
@@ -1,10 +1,10 @@
 SET PATH=%PATH%;C:\Program Files\Java\jdk1.5.0_16\bin
 SET TOMCAT_MASSBANK=C:\Program Files\Tomcat 5.5\webapps\ROOT\WEB-INF\classes\massbank\
 del *.class
+javac -cp .;build "%TOMCAT_MASSBANK%GetConfig.java" -d ./build
 javac -cp .;build "%TOMCAT_MASSBANK%ResultRecord.java" -d ./build
 javac -cp .;build "%TOMCAT_MASSBANK%ResultList.java" -d ./build
 javac -cp .;build "%TOMCAT_MASSBANK%MassBankCommon.java" -d ./build
-javac -cp .;build "%TOMCAT_MASSBANK%GetConfig.java" -d ./build
 javac -cp .;build canvas/DrawPane.java -d ./build
 javac -cp .;build draw2d/MOLformat.java -d ./build
 javac -cp .;build metabolic/MolFigure.java -d ./build
