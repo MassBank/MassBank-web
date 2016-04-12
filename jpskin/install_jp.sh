e6bfc1aeaa282429e2696cd6e1ec0820d2072295
Tobias Schulze <tobias.schulze@ufz.de>
2016-04-12 13:33:31 +0200

    Remove jpskin

--- jpskin/install_jp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo cp /vagrant/jpskin/htmlfiles/index.jsp /var/lib/tomcat7/webapps/MassBank/jsp/index.jsp;
-sudo cp /vagrant/jpskin/images/* -t /var/www/html/MassBank/img;
-sudo mkdir /var/www/html/MassBank/en;
-sudo cp /vagrant/jpskin/htmlfiles/*.html -t /var/www/html/MassBank/en;
-sudo cp /vagrant/jpskin/cssfiles/* -t /var/www/html/MassBank/css;
-sudo cp /vagrant/jpskin/scriptfiles/* -t /var/www/html/MassBank/script;
-sudo mkdir /var/www/html/MassBank/photo;
-sudo cp /vagrant/jpskin/photo/* -t /var/www/html/MassBank/photo;
