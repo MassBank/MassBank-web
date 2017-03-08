f230a5d82360c8ac2db069844593180975e0e6b5
marcstoehr <marc.stoehr@uni-leipzig.de>
2017-03-08 21:11:09 +0100

    modified modules/apache/htdocs/MassBank/script/massbank_specktackle.js to retrieve peak data from the database and pass them along with the id to SpeckTackle

--- modules/apache/htdocs/MassBank/script/massbank_spectackle.js
+++ modules/apache/htdocs/MassBank/script/massbank_spectackle.js
@@ -23,3 +23,39 @@ function loadSpectrum(spectrum) {
 
 }
 
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+$(document).ready(function (){
+		
+	// load the spectrum
+	var queryString = window.location.href.slice(window.location.href.indexOf('?') + 1);
+	$.get("../cgi-bin/GetData.cgi" + queryString).
+		done(function (data) {
+			var spectrum = {};
+			var urlVars = getUrlVars();
+			var line = "";
+			spectrum["spectrumId"] = urlVars["id"];
+			spectrum["peaks"] = [];
+			dataLines = data.split("\n");
+			for (index = 0; index < dataLines.length(); ++index) {
+				line = dataLines[index].split("\t");
+				spectrum["peaks"][index] = {"mz":line[0],"intensity":line[2]};
+			}
+			spectrum["mzStart"] = 0;
+			spectrum["mzStop"] = 200;
+			loadSpectrum(spectrum);
+		});
+});
+
