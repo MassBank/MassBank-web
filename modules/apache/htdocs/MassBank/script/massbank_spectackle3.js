5e2bef45377c1cb68a46fea0687a800242447176
marcstoehr <marc.stoehr@uni-leipzig.de>
2017-04-03 11:35:17 +0200

    added the spectrum display capability

--- modules/apache/htdocs/MassBank/script/massbank_spectackle3.js
+++ modules/apache/htdocs/MassBank/script/massbank_spectackle3.js
@@ -1,4 +1,30 @@
-var MSchart = st.chart.ms();
+// MS spectackle variables
+var MSchart;
+var MSData;
+
+function initializeMSSpecTackle() {
+
+    if (MSchart != undefined) return;
+
+    MSchart = st.chart.ms().labels(true).margins([10,60,30,90]);
+
+    MSchart.render("#spectrum_canvas");
+
+    MSData = st.data.set().x("peaks.mz").y("peaks.intensity").title("spectrumId");
+
+    MSchart.load(MSData);
+
+}
+
+function loadSpectrum(spectrum) {
+
+    initializeMSSpecTackle();
+
+    MSData.add(spectrum);
+
+    var urlVars = getUrlVars();
+
+}
 
 // Read a page's GET URL variables and return them as an associative array.
 function getUrlVars()
@@ -55,5 +81,43 @@ $(document).ready(function (){
             // spinner.css('display', 'none');
         });
     });
+
+    // load the spectrum
+    var queryString = window.location.href.slice(window.location.href.indexOf('?') + 1);
+    var jqxhr = $.get("../cgi-bin/GetData.cgi?" + queryString,"text");
+    jqxhr.done(function (data) {
+        var spectrum = {};
+        var urlVars = getUrlVars();
+        var line = "";
+        var mzStart = null;
+        var mzStop = null;
+        spectrum["spectrumId"] = urlVars["id"];
+        spectrum["peaks"] = [];
+        dataLines = data.split("\n");
+        for (index = 0; index < dataLines.length; ++index) {
+            if (dataLines[index]) {
+                line = dataLines[index].split("\t");
+                spectrum["peaks"][index] = {"mz":parseFloat(line[0]),"intensity":parseFloat(line[2])};
+                if (index == 0) {
+                    mzStart = parseFloat(line[0]);
+                    mzStop = parseFloat(line[2]);
+                } else {
+                    if (mzStart > parseFloat(line[0])) {
+                        mzStart = parseFloat(line[0]);
+                    }
+                    if (mzStop < parseFloat(line[0])) {
+                        mzStop = parseFloat(line[0]);
+                    }
+                }
+            }
+        }
+        spectrum["mzStart"] = mzStart;
+        spectrum["mzStop"] = mzStop;
+        loadSpectrum(spectrum);
+    });
+    jqxhr.fail(function (jqXHR, textStatus, errorThrown) {
+        alert("Error: " + errorThrown);
+    });
+
 });
 
