9ed074b6113847b28008aff06f0db698471b31ab
Tobias Schulze <tobias.schulze@ufz.de>
2017-07-04 15:23:41 +0200

    Helps against paranoia...

--- Setup_MassBank.bat
+++ Setup_MassBank.bat
@@ -1,18 +1,23 @@
-@echo off
-setlocal
+@ECHO OFF
 Set /p MBUSERNAME= Please set an username for the MassBank administration tool: 
 
+:PW
+REM Powershell script based on https://stackoverflow.com/questions/19950620/how-to-hide-password-in-command-line-with-and-get-the-value-into-bat-file (License CC BY-CA 3.0)
+set "psCommand=powershell -Command "$pword = read-host 'Please set a password for the MassBank administration tool' -AsSecureString ; ^
+		$BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+		[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set PASSWORD=%%p
 
-:pw
-Set /p PASSWORD= Please set a password for the MassBank administration tool: 
-Set /p PASSWORD1= Please repeat the password for the MassBank administration tool: 
+set "psCommand=powershell -Command "$pword = read-host 'Please repeat the password' -AsSecureString ; ^
+		$BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+		[System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set PASSWORD1=%%p
+
 If %PASSWORD%==%PASSWORD1% ( goto start) else ( goto pw_repeat)
 
 :pw_repeat
 echo The passwords did not match. Repeat please.
-Set /p PASSWORD= Please set a password for the MassBank administration tool: 
-Set /p PASSWORD1= Please repeat the password for the MassBank administration tool: 
-If %PASSWORD%==%PASSWORD1% ( goto start) else ( goto pw_repeat)
+goto PW
 
 :start
 
