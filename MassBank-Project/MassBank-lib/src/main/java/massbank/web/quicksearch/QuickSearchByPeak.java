3346f52446a0ec6b2feb4573e3b46b00098ee332
Treutler <hendrik.treutler@gmail.com>
2018-06-13 13:38:37 +0200

    Added MassBank manuals

--- MassBank-Project/MassBank-lib/src/main/java/massbank/web/quicksearch/QuickSearchByPeak.java
+++ MassBank-Project/MassBank-lib/src/main/java/massbank/web/quicksearch/QuickSearchByPeak.java
@@ -2,6 +2,9 @@ package massbank.web.quicksearch;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,6 +21,16 @@ public class QuickSearchByPeak implements SearchFunction {
 
 	public ArrayList<String> search(Connection connection) {
 		ArrayList<String> resList = new ArrayList<String>();
+//		System.out.println("############################################");
+//		System.out.println(this.request);
+//		System.out.println(this.request.getRequestURL());
+//		System.out.println(this.request.getQueryString());
+//		Map<String, String[]> parameterMap = request.getParameterMap();
+//		for (Entry<String, String[]> e : parameterMap.entrySet()) {
+//			String strKey = e.getKey().toUpperCase();
+//			String[] strVal = e.getValue();
+//			System.out.println(strKey + "\t" + Arrays.toString(strVal));
+//		}
 		Search search = new Search(this.request, connection);
 		resList = search.getResult();
 		return resList;
