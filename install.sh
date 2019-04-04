53c51fc71afd2c32565778e473c783f9abe1bdad
René Meier <rmeier@ipb-halle.de>
2019-04-04 12:25:49 +0200

    Add support for automatic rollout of MassBank via docker-compose

--- install.sh
+++ install.sh
@@ -1,112 +1,46 @@
-#!/bin/sh
-#------------------------------------------------
-# MassBank System Installer
-#------------------------------------------------
+#!/bin/bash
+CURRENT_UID=$(id -u):$(id -g)
+export CURRENT_UID
 
-echo
-echo
-echo "*********************************************"
-echo "* MassBank System Installer                 *"
-echo "*  version 1.8.1                            *"
-echo "*********************************************"
-echo
 
-echo "Installation of MassBank modules."
-echo -n "Are you sure(yes/no)? "
-read ANSWER
-if [ $ANSWER != "yes" ] && [ $ANSWER != "Yes" ] && [ $ANSWER != "YES" ] && [ $ANSWER != "y" ] && [ $ANSWER != "Y" ];
-then
-    echo
-    echo "Install cancel."
-    echo
-    exit 0
-fi
+case $2 in
+[0-9])
+	TAG=$2
+	export TAG
+        ;;
+*)
+        echo "Usage: install.sh <operation> <instance>"
+        echo "         <operation> ... start, stop or refresh"
+        echo "         <instance>  ... 0 to 9"
+        exit 1
+        ;;
+esac
 
-echo
-echo "Starting install..."
-echo
+case $1 in
+start)
+	docker-compose -f compose/full-service.yml -p $2 pull 
+	docker-compose -f compose/full-service.yml -p $2 run --rm maven mvn -Duser.home=/var/maven -f /project package
+	docker-compose -f compose/full-service.yml -p $2 up -d tomcat
+	echo "Wait 20s for database to be ready..."
+	sleep 20
+	docker-compose -f compose/full-service.yml -p $2 \
+		run --rm dbupdate \
+		/project/MassBank-lib/target/MassBank-lib-0.0.1-default/MassBank-lib-0.0.1/bin/RefreshDatabase
+	;;
+stop)
+	docker-compose -f compose/full-service.yml -p $2 stop
+	docker-compose -f compose/full-service.yml -p $2 rm
+	;;
+refresh)
+	docker-compose -f compose/full-service.yml -p $2 \
+                run --rm dbupdate \
+                /project/MassBank-lib/target/MassBank-lib-0.0.1-default/MassBank-lib-0.0.1/bin/RefreshDatabase
+	;;
+*)
+	echo "Usage: install.sh <operation> <instance>"
+	echo "         <operation> ... start, stop or refresh"
+	echo "         <instance>  ... 0 to 9"
+	exit 1
+	;;
+esac
 
-# Install Files Path
-INST_ROOT_PATH=./modules
-INST_HTDOCS_PATH=$INST_ROOT_PATH/apache/htdocs
-INST_ERROR_PATH=$INST_ROOT_PATH/apache/error
-INST_CONF_PATH=$INST_ROOT_PATH/apache/conf
-INST_MODULE_PATH=$INST_ROOT_PATH/apache/modules
-INST_TOMCAT_PATH=$INST_ROOT_PATH/tomcat
-INST_SQL_PATH=$INST_ROOT_PATH/sql/init.sql
-
-# Apache Path
-APACHE_HTDOCS_PATH=/var/www/html
-APACHE_ERROR_PATH=/var/www/error
-APACHE_CONF_PATH=/etc/httpd/conf
-APACHE_MODULE_PATH=/usr/lib/httpd/modules
-
-# Tomcat Path
-DEST_TOMCAT_PATH=/usr/local
-
-echo
-echo ">> service stop"
-/etc/init.d/xvfb stop 2> /dev/null
-/etc/init.d/tomcat stop 2> /dev/null
-/etc/init.d/httpd stop
-
-echo
-echo ">> file copy"
-cp -rp $INST_HTDOCS_PATH/* $APACHE_HTDOCS_PATH
-cp -ip $INST_ROOT_PATH/massbank.conf $APACHE_HTDOCS_PATH/MassBank
-cp -p $INST_ERROR_PATH/40?.html $APACHE_ERROR_PATH
-cp -p $INST_ERROR_PATH/50?.html $APACHE_ERROR_PATH
-cp -rip $INST_CONF_PATH/* $APACHE_CONF_PATH
-cp -ip $INST_CONF_PATH/.htpasswd $APACHE_CONF_PATH
-cp -ip $INST_MODULE_PATH/mod_jk.so $APACHE_MODULE_PATH
-cp -rp $INST_TOMCAT_PATH $DEST_TOMCAT_PATH
-cp -ip $INST_ROOT_PATH/StartupScript/tomcat /etc/init.d
-cp -ip $INST_ROOT_PATH/StartupScript/xvfb /etc/init.d
-mv $APACHE_ERROR_PATH/noindex.html $APACHE_ERROR_PATH/noindex.html.bak
-cp -p $INST_ERROR_PATH/403.html $APACHE_ERROR_PATH/noindex.html
-
-echo
-echo ">> change file mode"
-chmod a+x $APACHE_HTDOCS_PATH/MassBank/cgi-bin/*.cgi
-chmod a+x $APACHE_HTDOCS_PATH/MassBank/cgi-bin/*/*.pl
-chmod 755 $APACHE_HTDOCS_PATH/MassBank/script/*.pl
-chmod 777 $APACHE_HTDOCS_PATH/MassBank/StructureSearch/struct.dat
-chmod 755 $APACHE_HTDOCS_PATH/MassBank/StructureSearch/struct_server
-chmod 777 $APACHE_HTDOCS_PATH/MassBank/StructureSearch/temp
-chmod 777 $APACHE_HTDOCS_PATH/MassBank/StructureSearch
-chmod u+x $APACHE_MODULE_PATH/mod_jk.so
-chmod u+x $DEST_TOMCAT_PATH/tomcat/bin/*.sh
-chmod a+w $DEST_TOMCAT_PATH/tomcat/temp
-chmod u+x /etc/init.d/tomcat
-chmod u+x /etc/init.d/xvfb
-
-echo
-echo ">> service start"
-/etc/init.d/xvfb start
-/etc/init.d/mysqld start
-/etc/init.d/tomcat start
-/etc/init.d/httpd start
-
-echo
-echo ">> create database (root authority)"
-mysql --user=root -p < $INST_SQL_PATH
-
-echo
-echo ">> chkconfig xvfb on"
-echo ">> chkconfig mysqld on"
-echo ">> chkconfig httpd on"
-echo ">> chkconfig tomcat on"
-chkconfig xvfb on
-chkconfig mysqld on
-chkconfig httpd on
-chkconfig tomcat on
-
-echo
-echo
-echo
-echo "Done."
-echo
-echo "Please edit \"FrontServer URL\" of \""$APACHE_HTDOCS_PATH"/MassBank/massbank.conf\" appropriately."
-echo
-echo
-echo
