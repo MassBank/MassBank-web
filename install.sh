388f632a575e0200f53911a51baf8ab6618f8a4d
René Meier <rmeier@ipb-halle.de>
2019-04-24 16:52:59 +0200

    Add option for code redeploy.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ case $2 in
         ;;
 *)
         echo "Usage: install.sh <operation> <instance>"
-        echo "         <operation> ... start, stop or refresh"
+        echo "         <operation> ... start, stop, redeploy or refresh"
         echo "         <instance>  ... 0 to 9"
         exit 1
         ;;
@@ -18,27 +18,33 @@ esac
 
 case $1 in
 start)
-	docker-compose -f compose/full-service.yml -p $2 pull 
-	docker-compose -f compose/full-service.yml -p $2 run --rm maven mvn -Duser.home=/var/maven -f /project package
-	docker-compose -f compose/full-service.yml -p $2 up -d tomcat
+	docker-compose -f compose/full-service.yml -p $TAG pull 
+	docker-compose -f compose/full-service.yml -p $TAG run --rm maven mvn -Duser.home=/var/maven -f /project package
+	docker-compose -f compose/full-service.yml -p $TAG up -d tomcat
 	echo "Wait 20s for database to be ready..."
 	sleep 20
-	docker-compose -f compose/full-service.yml -p $2 \
+	docker-compose -f compose/full-service.yml -p $TAG \
 		run --rm dbupdate \
 		/project/MassBank-lib/target/MassBank-lib-0.0.1-default/MassBank-lib-0.0.1/bin/RefreshDatabase
 	;;
 stop)
-	docker-compose -f compose/full-service.yml -p $2 stop
-	docker-compose -f compose/full-service.yml -p $2 rm
+	docker-compose -f compose/full-service.yml -p $TAG rm -s
 	;;
 refresh)
-	docker-compose -f compose/full-service.yml -p $2 \
+	docker-compose -f compose/full-service.yml -p $TAG \
                 run --rm dbupdate \
                 /project/MassBank-lib/target/MassBank-lib-0.0.1-default/MassBank-lib-0.0.1/bin/RefreshDatabase
 	;;
+redeploy)
+	docker-compose -f compose/full-service.yml -p $TAG pull
+	docker-compose -f compose/full-service.yml -p $TAG run --rm maven mvn -Duser.home=/var/maven -f /project package
+	docker-compose -f compose/full-service.yml -p $TAG rm -s tomcat
+	docker-compose -f compose/full-service.yml -p $TAG up -d tomcat
+	;;
+
 *)
 	echo "Usage: install.sh <operation> <instance>"
-	echo "         <operation> ... start, stop or refresh"
+	echo "         <operation> ... start, stop, redeploy or refresh"
 	echo "         <instance>  ... 0 to 9"
 	exit 1
 	;;
