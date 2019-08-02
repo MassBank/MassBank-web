abb96565c7d7189f58a5ac965f61864696b3556a
meier-rene <rmeier@googlemail.com>
2019-08-02 14:42:15 +0200

    Fix problems with RecordDisplay. Be more strict at file end.

--- MassBank-Project/MassBank-lib/src/main/java/massbank/RecordParserDefinition.java
+++ MassBank-Project/MassBank-lib/src/main/java/massbank/RecordParserDefinition.java
@@ -102,7 +102,7 @@ public class RecordParserDefinition extends GrammarDefinition {
 		// Last line of a MassBank Record is // .
 		def("tagsep", StringParser.of(": "));
 		def("valuesep", StringParser.of("; "));
-		def("endtag", StringParser.of("//").trim());
+		def("endtag", StringParser.of("//").seq(Token.NEWLINE_PARSER));
 		def("multiline_start", StringParser.of("  "));
 		
 		def("uint_primitive", digit().plus().flatten());
