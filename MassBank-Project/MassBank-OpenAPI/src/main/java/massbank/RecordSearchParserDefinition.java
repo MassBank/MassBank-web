3c8a58c6befe6bb60f78aeded9e7986a5bc4070d
René Meier <rmeier@ipb-halle.de>
2020-02-10 15:05:45 +0100

    Continue API

--- MassBank-Project/MassBank-OpenAPI/src/main/java/massbank/RecordSearchParserDefinition.java
+++ MassBank-Project/MassBank-OpenAPI/src/main/java/massbank/RecordSearchParserDefinition.java
@@ -1,26 +1,79 @@
 package massbank;
 
-import java.util.function.Function;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.petitparser.context.Context;
-import org.petitparser.context.Result;
+import org.petitparser.parser.primitive.CharacterParser;
 import org.petitparser.parser.primitive.StringParser;
 import org.petitparser.tools.GrammarDefinition;
 
 public class RecordSearchParserDefinition extends GrammarDefinition {
 	private static final Logger logger = LogManager.getLogger(RecordSearchParserDefinition.class);
-	
+
 	public RecordSearchParserDefinition() {
 		def("start",
-				ref("accession")
-				.end()
+			ref("name")
+			.end()
 		);
-		
-		def("accession", 
-				StringParser.of("ACCESSION")
+
+		//Comparison operators in FIQL notation or alternative notation:
+		//Equal to : ==
+		def("eq", StringParser.of("=="));
+		//Not equal to : !=
+		def("ne", StringParser.of("!="));
+		//Less than : =lt=
+		def("lt", StringParser.of("=lt=").or(StringParser.of("<")));
+		//Less than or equal to : =le=
+		def("le", StringParser.of("=le=").or(StringParser.of("<=")));
+		//Greater than operator : =gt=
+		def("gt", StringParser.of("=gt=").or(StringParser.of(">")));
+		//Greater than or equal to : =ge=
+		def("ge", StringParser.of("=ge=").or(StringParser.of(">=")));
+		//In : =in=
+		def("in", StringParser.of("=in="));
+		//Not in : =out=
+		def("out", StringParser.of("=out="));
+
+
+		def("input",
+			StringParser.of("ACCESSION")
 		);
+
+		def("constraint",
+
+			StringParser.of("ACCESSION")
+		);
+
+
+		def("comparison",
+			ref("selector")
+			.seq(ref("comparison-op"))
+			.seq(ref("arguments"))
+		);
+
+		def("selector",
+			ref("unreserved-str")
+		);
+
+		def("comparison-op",
+			ref("comp-fiql")
+			.or(ref("comp-alt"))
+		);
+
+		def("name",
+			StringParser.of("name")
+			.seq(ref("eq"))
+			.seq(StringParser.of("nicotine"))
+		);
+
+		def("value",
+			ref("unreserved-str")
+			.or(ref("double-quoted"))
+			.or(ref("single-quoted"))
+		);
+		def("unreserved-str",
+			CharacterParser.any()
+		);
+
 	}
 }
 
@@ -30,14 +83,9 @@ public class RecordSearchParserDefinition extends GrammarDefinition {
 //and            = constraint, { ";" , constraint };
 //constraint     = ( group | comparison );
 //group          = "(", or, ")";
-//comparison     = selector, comparison-op, arguments;
-//selector       = unreserved-str;
-//comparison-op  = comp-fiql | comp-alt;
-//comp-fiql      = ( ( "=", { ALPHA } ) | "!" ), "=";
-//comp-alt       = ( ">" | "<" ), [ "=" ];
+
+
 //arguments      = ( "(", value, { "," , value }, ")" ) | value;
-//value          = unreserved-str | double-quoted | single-quoted;
-//
 //unreserved-str = unreserved, { unreserved }
 //single-quoted  = "'", { ( escaped | all-chars - ( "'" | "\" ) ) }, "'";
 //double-quoted  = '"', { ( escaped | all-chars - ( '"' | "\" ) ) }, '"';
