b44a2623c1aaedc3cd31150b33445415f2b97186
René Meier <rmeier@ipb-halle.de>
2020-10-22 18:33:00 +0200

    Add as requested in #265

--- MassBank-Project/MassBank-lib/src/main/resources/create_massbank_scheme.sql
+++ MassBank-Project/MassBank-lib/src/main/resources/create_massbank_scheme.sql
@@ -249,3 +249,58 @@ CREATE TABLE LAST_UPDATE (
 	TIME TIMESTAMP,
 	VERSION VARCHAR(100)
 );
+
+create view spectra_data as select
+       RECORD.ACCESSION as spectrum_id,
+       RECORD.RECORD_TITLE as spectrum_name,
+       RECORD.DATE as date,
+       RECORD.AUTHORS as authors,
+       RECORD.LICENSE as license,
+       RECORD.COPYRIGHT as copyright,
+       RECORD.PUBLICATION as publication,
+       RECORD.AC_MASS_SPECTROMETRY_MS_TYPE as msLevel,
+       RECORD.AC_MASS_SPECTROMETRY_ION_MODE as polarity,
+       RECORD.PK_SPLASH as splash,
+       RECORD.CH as compound_id,
+       (select VALUE from MS_FOCUSED_ION where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'BASE_PEAK') as precursorIntensity,
+       (select VALUE from MS_FOCUSED_ION where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'PRECURSOR_M/Z') as precursorMz,
+       (select VALUE from MS_FOCUSED_ION where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'PRECURSOR_TYPE') as adduct,
+       (select VALUE from AC_MASS_SPECTROMETRY where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'IONIZATION') as ionization,
+       (select VALUE from AC_MASS_SPECTROMETRY where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'IONIZATION_VOLTAGE') as ionizationVoltage,
+       (select VALUE from AC_MASS_SPECTROMETRY where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'FRAGMENTATION_MODE') as fragmentationMode,
+       (select VALUE from AC_MASS_SPECTROMETRY where RECORD = RECORD.ACCESSION
+       and SUBTAG = 'COLLISION_ENERGY') as collisionEnergyText,
+       (select INSTRUMENT.AC_INSTRUMENT from INSTRUMENT where
+       INSTRUMENT.ID = RECORD.AC_INSTRUMENT) as instrument,
+       (select INSTRUMENT.AC_INSTRUMENT_TYPE from INSTRUMENT where
+       INSTRUMENT.ID = RECORD.AC_INSTRUMENT) as instrument_type
+from RECORD;
+
+create view compound as select
+       COMPOUND.ID as compound_id,
+       COMPOUND.CH_FORMULA as formula,
+       COMPOUND.CH_EXACT_MASS as exactmass,
+       COMPOUND.CH_SMILES as smiles,
+       COMPOUND.CH_IUPAC as inchi,
+       (select DATABASE_ID from CH_LINK where CH_LINK.COMPOUND = COMPOUND.ID
+       and CH_LINK.DATABASE_NAME = 'INCHIKEY') as inchikey,
+       (select DATABASE_ID from CH_LINK where CH_LINK.COMPOUND = COMPOUND.ID
+       and CH_LINK.DATABASE_NAME = 'CAS') as cas,
+       (select DATABASE_ID from CH_LINK where CH_LINK.COMPOUND = COMPOUND.ID
+       and CH_LINK.DATABASE_NAME = 'PUBCHEM') as pubchem,
+       NAME.CH_NAME as compound_name
+from COMPOUND
+join COMPOUND_NAME on (COMPOUND.ID = COMPOUND_NAME.COMPOUND)
+join NAME on (COMPOUND_NAME.NAME = NAME.ID);
+
+create view peaks as select
+       PEAK.RECORD as spectrum_id,
+       PEAK.PK_PEAK_MZ as mz,
+       PEAK.PK_PEAK_INTENSITY as intensity
+from PEAK;
