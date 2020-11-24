e6b1063d67836cb24e01ccb1169bebdc96457c5b
René Meier <rmeier@ipb-halle.de>
2020-11-24 23:35:58 +0100

    Script to create additional release files.

---
+++ create-releasefiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker-compose -f compose/mariadb-docker-compose.yml stop
+docker-compose -f compose/mariadb-docker-compose.yml rm
+docker-compose -f compose/mariadb-docker-compose.yml pull
+docker-compose -f compose/mariadb-docker-compose.yml up -d
+cd MassBank-Project
+mvn package
+./MassBank-lib/target/MassBank-lib/MassBank-lib/bin/RefreshDatabase
+echo Create sql dump...
+mysqldump -u root -h 127.0.0.1 -p --databases MassBank > MassBank.sql
+./MassBank-lib/target/MassBank-lib/MassBank-lib/bin/RecordExporter -o MassBank.msp ../../MassBank-data/*
