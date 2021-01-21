6745708ac325ede00ae2d502834a8b0b5bc48950
Tobias Schulze <tobias.schulze@ufz.de>
2021-01-21 14:03:07 +0100

    Add new piwik.js

---
+++ MassBank-Project/MassBank/src/main/webapp/js/piwik.js
@@ -0,0 +1,19 @@
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setCookieDomain", "*.massbank.eu"]);
+  _paq.push(["setDomains", ["*.massbank.ufz.de","*.massbank.eu","*.massbank.eu","*.massbank.ufz.de"]]);
+  _paq.push(["disableCookies"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://www.ufz.de/stats/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '24']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
