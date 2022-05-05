1594b69ed37b5002ca9913e8ed4c316f2f1ce48a
René Meier <rmeier@ipb-halle.de>
2022-05-05 13:34:54 +0200

    Implement GET /record/{Id}

--- MassBank-Project/MassBank-OpenAPI/src/main/java/massbank/openapi/api/RecordApiDelegateImpl.java
+++ MassBank-Project/MassBank-OpenAPI/src/main/java/massbank/openapi/api/RecordApiDelegateImpl.java
@@ -1,5 +1,6 @@
 package massbank.openapi.api;
 
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import massbank.Record;
 import massbank.db.DatabaseManager;
 
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -16,24 +18,7 @@ import java.util.List;
  */
 @Service
 public class RecordApiDelegateImpl implements RecordApiDelegate {
-    /**
-     * DELETE /record/{recordId} : delete a record
-     * deletes the record with the specified id from the massBank database
-     *
-     * @param recordId The record id (required)
-     * @return the record was successfully deleted (status code 204)
-     *         or Access token is missing or invalid (status code 401)
-     *         or Access token does not allow the operation (status code 403)
-     *         or The record with the recordId was not found (status code 404)
-     *         or There was in internal error during database access (status code 500)
-     * @see RecordApi#deleteRecord
-     */
-    public ResponseEntity<Void> deleteRecord(String recordId) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-
-    }
-
-    /**
+	/**
      * GET /record/{recordId} : Returns the content of one record
      * Returns the content of the record in MassBank format
      *
@@ -44,88 +29,32 @@ public class RecordApiDelegateImpl implements RecordApiDelegate {
      * @see RecordApi#getRecord
      */
     public ResponseEntity<String> getRecord(String recordId) {
-    	Record record = null;
-        // load record from database
-        DatabaseManager dbMan;
-        try {
-                dbMan = new DatabaseManager("MassBank");
-                record = dbMan.getAccessionData(recordId);
-                dbMan.closeConnection();
-        } catch (SQLException | ConfigurationException e) {
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Database connection error").build();
-        }
-        if (record == null) {
-                return Response.status(Response.Status.NOT_FOUND).entity("Record \"" + id + "\" not found").build();
-        }
-
-        String recordstring = record.toString();
-        return ResponseEntity.ok(recordstring);
-    }
-
-    /**
-     * GET /record : Returns all records matching the search
-     * This resource reports all record ids which fulfill the criteria in the search parameters, or all records if the query parameters are missing or empty
-     *
-     * @param name Search records with the specified string in RECORD_TITLE or CH$NAME. (optional)
-     * @param formula Search records with the specified formula CH$FORMULA. (optional)
-     * @return successful operation (status code 200)
-     *         or The record or query was invalid (status code 400)
-     *         or There was in internal error during database access (status code 500)
-     * @see RecordApi#getRecordsIds
-     */
-    public ResponseEntity<List<String>> getRecordsIds(String name,
-        String formula) {
         getRequest().ifPresent(request -> {
             for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
-                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
-                    String exampleString = "\"PR030008\"";
-                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                if (mediaType.isCompatibleWith(MediaType.valueOf(""))) {
+                    String exampleString = "";
+                    ApiUtil.setExampleResponse(request, "", exampleString);
                     break;
                 }
             }
         });
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-
+        Record record = null;
+		// load record from database
+		DatabaseManager dbMan;
+		try {
+			dbMan = new DatabaseManager("MassBank");
+			record = dbMan.getAccessionData(recordId);
+			dbMan.closeConnection();
+		} catch (SQLException | ConfigurationException e) {
+			return new ResponseEntity<>("Database connection error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if (record == null) {
+			return new ResponseEntity<>("Record \"" + recordId + "\" not found", HttpStatus.NOT_FOUND);
+		}
+		String recordstring = record.toString();
+		return new ResponseEntity<>(recordstring, HttpStatus.OK);
     }
-
-    /**
-     * POST /record : Uploads a record to the database
-     * Uploads a new record to the massBank database if the ACCESSION in the record is unique
-     *
-     * @param body  (required)
-     * @return Created a new record (status code 201)
-     *         or Access token is missing or invalid (status code 401)
-     *         or Access token does not allow the operation (status code 403)
-     *         or Could not insert dataset, because it already exists (status code 409)
-     *         or The record or query was invalid (status code 400)
-     *         or There was in internal error during database access (status code 500)
-     * @see RecordApi#postRecord
-     */
-    public ResponseEntity<Void> postRecord(String body) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-
-    }
-
-    /**
-     * PUT /record/{recordId} : replace a record
-     * replaces the record in with the specified id by a new record, which must have the same ACCESSION
-     *
-     * @param recordId The record id (required)
-     * @param body  (optional)
-     * @return the record was successfully replaced (status code 204)
-     *         or The record or query was invalid (status code 400)
-     *         or Access token is missing or invalid (status code 401)
-     *         or Access token does not allow the operation (status code 403)
-     *         or The record with the recordId was not found (status code 404)
-     *         or There was in internal error during database access (status code 500)
-     * @see RecordApi#replaceRecord
-     */
-    public ResponseEntity<Void> replaceRecord(String recordId,
-        String body) {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-
-    }
-
+	
 }
 
 /*
