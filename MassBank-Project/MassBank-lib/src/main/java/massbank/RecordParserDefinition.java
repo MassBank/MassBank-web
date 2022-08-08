9995db6b9aabd785b870e9cbb71e471cd98fd3fd
René Meier <rmeier@ipb-halle.de>
2022-08-08 15:18:07 +0200

    Change order of rules in PRECURSOR_TYPE for correct parsing.

--- MassBank-Project/MassBank-lib/src/main/java/massbank/RecordParserDefinition.java
+++ MassBank-Project/MassBank-lib/src/main/java/massbank/RecordParserDefinition.java
@@ -1424,9 +1424,10 @@ public class RecordParserDefinition extends GrammarDefinition {
 				CharacterParser.anyOf("+-")
 				.seq(ref("uint_primitive").optional())
 				.seq(
-					ref("molecular_formula")
-					.or(StringParser.of("ACN"))
+					
+					StringParser.of("ACN")
 					.or(StringParser.of("FA"))
+					.or(ref("molecular_formula"))
 				)
 //				.map((List<?> value) -> {
 //					System.out.println(value);
