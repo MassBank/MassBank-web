dcf0c935e897bfd9eb0211f90ccabae23f33b01e
René Meier <rmeier@ipb-halle.de>
2023-03-08 14:37:08 +0100

    Fixing issue #364. Adding support for Docker Compose V2

--- install.sh
+++ install.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+OUT=$(docker compose version --short  2> /dev/null)
+RET=$?
+if [ $RET -ne 0 ]; then
+        echo "Docker Compose V2 not found. Trying 'docker-compose'."
+	OUT2=$(docker-compose version --short  2> /dev/null)
+	RET2=$?
+	if [ $RET2 -ne 0 ]; then
+		echo "'docker-compose' not found."
+		echo "Expecting docker compose V2 or docker-compose"
+	else
+		if [[ $OUT2 = 1* ]]
+		then
+			echo "'docker-compose' version "$OUT" found. Using command 'docker-compose'." 
+			COMPOSE_COMMAND="docker-compose"
+		else
+			echo "'docker-compose' version "$OUT" found. Version 1.X expected."
+			exit
+		fi
+	fi
+else
+	if [[ $OUT = 2* ]]
+	then
+		echo "Docker Compose V2 version "$OUT" found. Using command 'docker compose'." 
+		COMPOSE_COMMAND="docker compose"
+	else
+		echo "Docker Compose V2 version "$OUT" found. Version 2.X expected."
+		exit
+	fi
+fi
+
 CURRENT_UID=$(id -u):$(id -g)
 export CURRENT_UID
 
@@ -22,29 +52,29 @@ esac
 
 case $1 in
 	start)
-		docker-compose -f compose/full-service.yml -p $TAG pull 
-		docker-compose -f compose/full-service.yml -p $TAG build
-		docker-compose -f compose/full-service.yml -p $TAG up -d mariadb
-		docker-compose -f compose/full-service.yml -p $TAG exec mariadb /root/waitforSQL.sh
-		CURRENT_UID=$(id -u):$(id -g) docker-compose -f compose/full-service.yml -p $TAG run --rm maven mvn -q -Duser.home=/var/maven -f /project clean package
-		docker-compose -f compose/full-service.yml -p $TAG up -d tomee
-		docker-compose -f compose/full-service.yml -p $TAG \
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG pull 
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG build
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG up -d mariadb
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG exec mariadb /root/waitforSQL.sh
+		CURRENT_UID=$(id -u):$(id -g) $COMPOSE_COMMAND -f compose/full-service.yml -p $TAG run --rm maven mvn -q -Duser.home=/var/maven -f /project clean package
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG up -d tomee
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG \
 			run --rm dbupdate \
 			/project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/RefreshDatabase
 	;;
 	stop)
-		docker-compose -f compose/full-service.yml -p $TAG rm -s
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG rm -s
 	;;
 	refresh)
-		docker-compose -f compose/full-service.yml -p $TAG \
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG \
 			run --rm dbupdate \
 			/project/MassBank-lib/target/MassBank-lib/MassBank-lib/bin/RefreshDatabase
 	;;
 	deploy)
-		docker-compose -f compose/full-service.yml -p $TAG pull
-		CURRENT_UID=$(id -u):$(id -g) docker-compose -f compose/full-service.yml -p $TAG run --rm maven mvn -q -Duser.home=/var/maven -f /project clean package
-		docker-compose -f compose/full-service.yml -p $TAG rm -s tomee
-		docker-compose -f compose/full-service.yml -p $TAG up -d tomee
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG pull
+		CURRENT_UID=$(id -u):$(id -g) $COMPOSE_COMMAND -f compose/full-service.yml -p $TAG run --rm maven mvn -q -Duser.home=/var/maven -f /project clean package
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG rm -s tomee
+		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG up -d tomee
 	;;
 	*)
 		usage
