1d6216a7d82e0b9722caad697a109fb0a7d568f0
René Meier <rmeier@ipb-halle.de>
2023-08-31 09:57:23 +0200

    Adjust bump-version.sh to the maven workaround

--- bump-version.sh
+++ bump-version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-mvn -f MassBank-Project/pom.xml build-helper:parse-version versions:set -DnewVersion=$1
-mvn -f MassBank-Project/pom.xml versions:set-property -Dproperty=timestamp -DnewVersion=`date --iso-8601=seconds`
+cd MassBank-Project
+mvn build-helper:parse-version versions:set -DnewVersion=$1
+mvn versions:set-property -Dproperty=timestamp -DnewVersion=`date --iso-8601=seconds`
