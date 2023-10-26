016ff0c36c37eed551de5a5ed32ef087d9d4176c
René Meier <rmeier@ipb-halle.de>
2023-10-26 09:37:52 +0200

    Use healthcheck.sh from mariadb developer instead of own script

--- install.sh
+++ install.sh
@@ -55,7 +55,6 @@ case $1 in
 		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG pull 
 		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG build
 		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG up -d mariadb
-		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG exec mariadb /root/waitforSQL.sh
 		CURRENT_UID=$(id -u):$(id -g) $COMPOSE_COMMAND -f compose/full-service.yml -p $TAG run --rm maven mvn -q -Duser.home=/var/maven clean package
 		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG up -d tomee
 		$COMPOSE_COMMAND -f compose/full-service.yml -p $TAG \
