dfd0181774ce9d491256fb41c49d464b2dc8f6b3
René Meier <meier.rene@googlemail.com>
2024-04-18 14:59:02 +0200

    Remove API stub. MassBank3 API is more advanced

--- MassBank-Project/MassBank-OpenAPI/src/main/java/massbank/RecordSearchParser.java
+++
@@ -1,9 +0,0 @@
-package massbank;
-
-import org.petitparser.tools.GrammarParser;
-
-public class RecordSearchParser extends GrammarParser {
-	public RecordSearchParser() {
-		    super(new RecordSearchParserDefinition());
-		  }
-}
